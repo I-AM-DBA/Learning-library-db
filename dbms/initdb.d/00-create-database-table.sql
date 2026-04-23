47f745eb1a7e73672e622e4b63f7f0f38c6d0fcf
johypark97 <johypark97@gmail.com>
2026-04-24 02:46:53 +0900

    feat: 3차 정규화 테이블에 부합하도록 테이블 생성 SQL 조정

--- dbms/initdb.d/00-create-database-table.sql
+++ dbms/initdb.d/00-create-database-table.sql
@@ -1,18 +1,44 @@
 CREATE DATABASE library;
 
-\c library
+\c library;
+
+-- create author table
+CREATE TABLE authors (
+    author_id          serial          PRIMARY KEY,
+    author_name        varchar(255)    NOT NULL
+);
+
+-- create category table
+CREATE TABLE categories (
+    category_id         serial          PRIMARY KEY,
+    category_name       varchar(255)    NOT NULL
+);
+
+-- create sub_category table
+CREATE TABLE sub_categories (
+    sub_category_id     serial          PRIMARY KEY,
+    category_id         integer         NOT NULL,
+    sub_category_name   varchar(255)    NOT NULL,
+    CONSTRAINT fk_sub_categories_category FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE RESTRICT
+);
+
+-- create publisher table
+CREATE TABLE publishers (
+    publisher_id        serial          PRIMARY KEY,
+    publisher_name      varchar(255)    NOT NULL,
+    publisher_phone     varchar(31)     NULL,
+    manager_name        varchar(255)    NULL
+);
 
 -- create books table
 CREATE TYPE books_status AS ENUM ('A', 'B');
 CREATE TABLE books (
     book_id             serial          PRIMARY KEY,
     title               varchar(255)    NOT NULL DEFAULT '',
-    publisher           varchar(255)    NULL,
+    publisher_id        integer         NULL,
     location_name       varchar(255)    NULL,
-    category            varchar(255)    NULL,
-    category_list       varchar(255)    NULL,
+    sub_category_id     integer         NULL,
     isbn                varchar(17)     NULL,
-    author              varchar(255)    NULL,
     created_date        date            NOT NULL DEFAULT CURRENT_DATE,
     published_year      smallint        NULL,
     status              books_status    NOT NULL DEFAULT 'A',
@@ -21,19 +47,41 @@ CREATE TABLE books (
     call_no             varchar(63)     NULL
 );
 
+-- create book_author_refs table
+CREATE TABLE book_author_refs (
+    auth_book_id    serial      PRIMARY KEY,
+    book_id         integer     NOT NULL,
+    author_id       integer     NOT NULL,
+    CONSTRAINT fk_book_author_refs_book FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE,
+    CONSTRAINT fk_book_author_refs_author FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
+);
+
 -- create users table
 CREATE TYPE users_type AS ENUM ('정회원', '준회원');
+CREATE TYPE users_status AS ENUM ('active', 'inactive', 'banned', 'sleep');
 CREATE TABLE users (
     user_id                 serial          PRIMARY KEY ,
-    email                   varchar(255)    NOT NULL,
-    password_hash           char(80)        NULL,
-    name                    varchar(100)    NOT NULL,
-    age                     date            NULL,
-    phone                   integer         NULL,
-    address                 varchar(255)    NULL,
     user_type               users_type      NOT NULL DEFAULT '준회원',
+    user_status             users_status    NOT NULL DEFAULT 'active',
     loan_available_date     date            NULL DEFAULT CURRENT_DATE,
     loaned_book_count       smallint        NOT NULL DEFAULT 0
+);
+
+-- create users_info table
+CREATE TABLE users_info (
+    user_id     integer         PRIMARY KEY,
+    name        varchar(255)    NOT NULL,
+    birth_date  date            NULL,
+    phone       varchar(31)     NULL,
+    address     varchar(255)    NULL,
+    CONSTRAINT fk_users_info_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+-- create users_secret table
+CREATE TABLE users_secret (
+    user_id         integer         PRIMARY KEY,
+    email           varchar(255)    NOT NULL,
+    password_hash   char(80)        NULL
 );
 
 -- create loans table
